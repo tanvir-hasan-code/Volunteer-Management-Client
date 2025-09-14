84b0ff7a1d87e645d1599ca1b02e5367ccac2c78
tanvir-hadan-code <tanvirx015@gmail.com>
2025-09-15 00:08:04 +0600

    Navbar Spacing Update

--- src/Layouts/Header/Navbar/Navbar.jsx
+++ src/Layouts/Header/Navbar/Navbar.jsx
@@ -63,7 +63,7 @@ const Navbar = () => {
     <div className={`navbar ${theme === "light"? 'bg-[#FFF5F2]': "bg-gray-600"} shadow-sm root-font lg:px-5`}>
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn pr-0 md:pr-4 btn-ghost lg:hidden">
+          <div tabIndex={0} role="button" className="btn pr-1 md:pr-4 btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
